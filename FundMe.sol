62a9bb763ac08eb2f6fd44356db774f67f1ddcbc
Mayokunnn <kareola960@gmail.com>
2025-03-04 13:10:34 +0100

    create a fund function and handle reverts

--- FundMe.sol
+++ FundMe.sol
@@ -2,4 +2,24 @@
 
 pragma solidity ^0.8.18;
 
-contract FundMe{}
+//Get funds from users
+//Withdraw funds
+//Set a minimum funding value in USD
+contract FundMe{
+    uint256 public myValue = 1;
+
+    function fund() public payable{
+        //Allow users to send funds
+        //Set a minimum funding value
+        myValue = myValue + 2;
+        require(msg.value > 1e18, "Didn't send enough ETH"	);
+
+        //What is a revert?
+        //Undo any actions that have been done, and send remaining gas back
+    }
+
+    // function withdraw() public {
+
+    // }
+
+}
