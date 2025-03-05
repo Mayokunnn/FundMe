558a805b105e1012a7e3792f00ff4329c818bd44
Mayokunnn <kareola960@gmail.com>
2025-03-05 18:48:13 +0100

    getting chainlink prices and solving solidity math

--- FundMe.sol
+++ FundMe.sol
@@ -2,24 +2,53 @@
 
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 //Get funds from users
 //Withdraw funds
 //Set a minimum funding value in USD
 contract FundMe{
-    uint256 public myValue = 1;
+    uint256 public minimumUsd = 5e18;
 
     function fund() public payable{
         //Allow users to send funds
         //Set a minimum funding value
-        myValue = myValue + 2;
-        require(msg.value > 1e18, "Didn't send enough ETH"	);
+        require(getConversionRate(msg.value) > minimumUsd, "Didn't send enough ETH"	);
 
         //What is a revert?
         //Undo any actions that have been done, and send remaining gas back
     }
 
-    // function withdraw() public {
+    function getLatestPrice() public view returns (uint256) {
+        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306); 
+       (
+            ,
+            int answer,
+            ,
+            ,
+        ) = priceFeed.latestRoundData();
 
-    // }
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getLatestPrice();
+        uint256 ethAmountInUsd = ( ethAmount * ethPrice) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function decimals() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306); 
+        return uint256(priceFeed.decimals());
+    }
+ 
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    // function withdraw() public {}
 
 }
