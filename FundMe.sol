964bbe8fc2b3ad479b0c912167d86bf221710276
Mayokunnn <kareola960@gmail.com>
2025-03-12 04:01:43 +0100

    reset funders and the amount funded after withdrawal

--- FundMe.sol
+++ FundMe.sol
@@ -27,6 +27,13 @@ contract FundMe{
     }
    
 
-    // function withdraw() public {}
+    function withdraw() public {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+    }
 
 }
